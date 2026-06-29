ec1baa8d1f334c645a193483e93319f162b6af98
Sacha Fulep <sachafulep@proton.me>
2026-06-29 22:01:21 +0200

    Undo change to keyboard shortcuts to prevent merge conflict

--- src/renderer/components/ft-shaka-video-player/ft-shaka-video-player.js
+++ src/renderer/components/ft-shaka-video-player/ft-shaka-video-player.js
@@ -2369,6 +2369,10 @@ export default defineComponent({
      * @param {KeyboardEvent} event
      */
     function keyboardShortcutHandler(event) {
+      if (!player || !hasLoaded.value) {
+        return
+      }
+
       if (document.activeElement.classList.contains('ft-input') || event.altKey) {
         return
       }
